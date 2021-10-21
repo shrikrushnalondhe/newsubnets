8eb0eaedfad13bbe1c3f88e80a9b41a14bd8c234
shrikrushnalondhe <89838542+shrikrushnalondhe@users.noreply.github.com>
2021-10-21 20:58:00 +0530

    Update newsubnets.tf

--- newsubnets.tf
+++ newsubnets.tf
@@ -31,6 +31,19 @@ output "aws_subnet_subnet_prod1" {
   value = "${aws_subnet.subnet_prod1.id}"
 }
 
+resource "aws_alb" "application_load_balancer" {
+  name               = "${var.app_name}-${var.app_environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public.*.id
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+
+  tags = {
+    Name        = "${var.app_name}-alb"
+    Environment = var.app_environment
+  }
+}
+
 terraform {
   required_providers {
     aws = {
